7a91fb9272205c66e59a0e7ee926581cf1c5bf1e
Nikita Merzlyakov <93618020+qwrtzz@users.noreply.github.com>
2024-06-12 19:30:25 +0300

    три дня дождя...

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// app.js
+
 function getWeather() {
     var city = document.getElementById("cityInput").value;
     var apiKey = '7e95f47ce3653c8160d2a14b870a7471'; 
@@ -25,6 +27,50 @@ function getWeather() {
             <p>Влажность: ${humidity}%</p>
             <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Погодные условия">
         `;
+        weatherCard.innerHTML = weatherInfo;
+    })
+    .catch(error => {
+        console.log("Ошибка при получении данных о погоде:", error);
+        weatherCard.innerHTML = "<h2>Не удалось получить данные о погоде. Попробуйте ещё раз.</h2>";
+    });
+}
+
+function getWeatherForThreeDays() {
+    var city = document.getElementById("cityInput").value;
+    var apiKey = '7e95f47ce3653c8160d2a14b870a7471'; 
+    var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + encodeURIComponent(city) + "&appid=" + apiKey + "&lang=ru&units=metric&cnt=3";
+
+    var weatherCard = document.getElementById('weatherCard');
+    weatherCard.innerHTML = '<h2>Загрузка...</h2>'; // Показать сообщение о загрузке
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if (data.cod !== "200") {
+            throw new Error(data.message);
+        }
+
+        var weatherInfo = '';
+        data.list.forEach(day => {
+            var date = new Date(day.dt * 1000);
+            var dateString = date.toLocaleDateString('ru-RU', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+            var weatherDescription = day.weather[0].description;
+            var temperature = day.main.temp;
+            var humidity = day.main.humidity;
+            var iconCode = day.weather[0].icon;
+            var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+
+            weatherInfo += `
+                <div class="day">
+                    <h3>${dateString}</h3>
+                    <p>Погодные условия: ${weatherDescription}</p>
+                    <p>Температура: ${temperature}°C</p>
+                    <p>Влажность: ${humidity}%</p>
+                    <img src="${iconUrl}" alt="Погодные условия">
+                </div>
+            `;
+        });
+
         weatherCard.innerHTML = weatherInfo;
     })
     .catch(error => {
